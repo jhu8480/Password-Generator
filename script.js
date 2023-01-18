bb98729b36fb23ec1a9c12d81451b44000f82194
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-01-18 13:53:01 -0500

    added prompts to get user inputs

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+// Starter code
+let generateBtn = document.querySelector("#generate");
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
+generateBtn.addEventListener("click", writePassword);
+
+// Logic to generate password:
+
+function generatePassword() {
+  // Validate the password length:
+  const passwordLength = prompt('How many characters do you need in your password?');
+  const isValidLength = validateLength(passwordLength);
+  if (!isValidLength) {
+    alert('The length is not valid, it must be at least 8 characters and no more than 128 characters!')
+    generatePassword();
+  }
+
+  // Ask the user about the type of characters to include
+  const numOfNumeric = prompt('How many numeric characters do you need?');
+  const numOfUpperCase = prompt('How many upper case letters do you need?');
+  const numOfLowerCase = prompt('How many upper case letters do you need?');
+  const numOfSpecialCharacters = prompt('How many special characters do you need?');
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function validateLength(number) {
+  return number > 7 & number < 129;
+}
+
+
