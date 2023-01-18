f0f4f44637054fd68556565c1efd5afb9880ce14
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-01-18 15:15:29 -0500

    added functions to generate random strings based on character types

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Starter code
-let generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
@@ -14,15 +14,22 @@ function generatePassword() {
   const passwordLength = prompt('How many characters do you need in your password?');
   const isValidLength = validateLength(passwordLength);
   if (!isValidLength) {
-    alert('The length is not valid, it must be at least 8 characters and no more than 128 characters!')
+    alert('The length is not valid, it must be at least 8 characters and no more than 128 characters!');
     generatePassword();
   }
 
   // Ask the user about the type of characters to include
-  const numOfNumeric = prompt('How many numeric characters do you need?');
-  const numOfUpperCase = prompt('How many upper case letters do you need?');
-  const numOfLowerCase = prompt('How many upper case letters do you need?');
-  const numOfSpecialCharacters = prompt('How many special characters do you need?');
+  const numOfNumeric = prompt(`How many numeric characters do you need? You can choose from 0 up to ${passwordLength} characters`);
+  let numOfCharactersRemain = passwordLength - numOfNumeric;
+  const numOfUpperCase = prompt(`How many upper case letters do you need? You can choose from 0 up to ${numOfCharactersRemain} characters`);
+  numOfCharactersRemain = numOfCharactersRemain - numOfUpperCase;
+  const numOfLowerCase = prompt(`How many upper case letters do you need? You can choose from 0 up to ${numOfCharactersRemain} characters`);
+  numOfCharactersRemain = numOfCharactersRemain - numOfLowerCase;
+  const numOfSpecialCharacters = prompt(`How many upper case letters do you need? You can choose from 0 up to ${numOfCharactersRemain} characters`);
+  
+  // Generate the password based on user's criteria
+  
+  
 
 }
 
@@ -30,4 +37,67 @@ function validateLength(number) {
   return number > 7 & number < 129;
 }
 
+function producePassword(numOfNumeric, numOfUpperCase, numOfLowerCase, numOfSpecialCharacters) {
+  const numericString = produceNumericString(numOfNumeric);
+  const upperCaseString = produceUpperCaseString(numOfUpperCase);
+  const lowerCaseString = produceLowerCaseString(numOfLowerCase);
+  const specialCharacterString = produceSpecialCharacters(numOfSpecialCharacters);
+
+  const stringArr = [];
+  stringArr.push(numericString);
+  stringArr.push(upperCaseString);
+  stringArr.push(lowerCaseString);
+  stringArr.push(specialCharacterString);
+  
+  // shuffle the order of elements here
+
+}
+
+function produceNumericString(numOfNumeric) {
+  const result = [];
+  const numericCharacters = '0123456789';
+  for (i = 0; i < numOfNumeric; i++) {
+    let randomIndex = Math.floor(Math.random() * 10);
+    const newStringPiece = numericCharacters.charAt(randomIndex);
+    result.push(newStringPiece);
+  }
+  console.log(result.join(''));
+  return result.join('');
+}
+
+function produceUpperCaseString(numOfUpperCase) {
+  const result = [];
+  const upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (i = 0; i < numOfUpperCase; i++) {
+    let randomIndex = Math.floor(Math.random() * 26);
+    const newStringPiece = upperCaseCharacters.charAt(randomIndex);
+    result.push(newStringPiece);
+  }
+  console.log(result.join(''));
+  return result.join('');
+}
+
+function produceLowerCaseString(numOfLowerCase) {
+  const result = [];
+  const lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  for (i = 0; i < numOfLowerCase; i++) {
+    let randomIndex = Math.floor(Math.random() * 26);
+    const newStringPiece = lowerCaseCharacters.charAt(randomIndex);
+    result.push(newStringPiece);
+  }
+  console.log(result.join(''));
+  return result.join('');
+}
+
+function produceSpecialCharacters(numOfSpecialCharacters) {
+  const result = [];
+  const specialCharacters = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~"`;
+  for (i = 0; i < numOfSpecialCharacters; i++) {
+    let randomIndex = Math.floor(Math.random() * specialCharacters.length);
+    const newStringPiece = specialCharacters.charAt(randomIndex);
+    result.push(newStringPiece);
+  }
+  console.log(result.join(''));
+  return result.join('');
+}
 
